23027a59f833c2fb7b468a211c6d4be8219de1f1
Lucas Romero <lucas.romero@jimdo.com>
2022-01-27 21:00:22 +0100

    Add url

--- action_plugins/jsonnet.py
+++ action_plugins/jsonnet.py
@@ -2,6 +2,8 @@
 # Copyright: (c) 2018, Ansible Project
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
+# https://github.com/luqasn/ansible_jsonnet_template_action
+
 from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
